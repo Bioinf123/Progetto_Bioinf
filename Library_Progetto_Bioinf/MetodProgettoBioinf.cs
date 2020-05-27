914a8e14ae3603c4edde1359c8e41af751c2bbfa
MATTEO BRANCALEONI <mattebranca2003@gmail.com>
2020-05-27 11:38:04 +0200

    cambiamenti

---
+++ Library_Progetto_Bioinf/MetodProgettoBioinf.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Library_Progetto_Bioinf
+{
+    public class MetodProgettoBioinf
+    {
+        public static string a = "resistente.txt";
+        public static string b = "sensibile.txt";
+        public static string ReadSens()
+        {
+            string lines = " ";
+            if (File.Exists(b))
+            {
+                using (StreamReader sr = new StreamReader(b))
+                {
+                    lines = sr.ReadToEnd();
+                }
+            }
+            return lines;
+        }
+
+        public static int Sensibile()
+        {
+            string lines = MetodProgettoBioinf.ReadSens();
+            int blength = lines.Length;
+            return blength;
+        }
+    }
+}
+
---
+++ Test_Progetto_Bioinf/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Progetto_Bioinf
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
